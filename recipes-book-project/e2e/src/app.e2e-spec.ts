89433f62e9bb7a3c54c35cf610b611b5d583b18a
Gabriel Caê <gccbounous@gmail.com>
2020-08-30 10:50:01 +0200

    now the recipe project is properly working

--- recipes-book-project/e2e/src/app.e2e-spec.ts
+++ recipes-book-project/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('recipes-book app is running!');
+    expect(page.getTitleText()).toEqual('recipes-book-project app is running!');
   });
 
   afterEach(async () => {
