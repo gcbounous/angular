490131a4beaa6942163738494006cb1b54c2ef73
Gabriel Caê <gccbounous@gmail.com>
2020-09-21 21:45:52 +0200

    sec-22 modules: created shopping-list module

--- recipes-book-project/src/app/app.module.ts
+++ recipes-book-project/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent }             from './app.component';
 import { HeaderComponent }          from './header/header.component';
-import { ShoppingListComponent }    from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent }    from './shopping-list/shopping-edit/shopping-edit.component';
 import { AuthComponent }            from './auth/auth.component'
-import { LoadingSpinnerComponent }  from 'src/shared/loading-spinner/loading-spinner.component';
+import { LoadingSpinnerComponent }  from '../shared/loading-spinner/loading-spinner.component';
 import { AlertComponent }           from '../shared//alert/alert.component';
 
 import { DropdownDirective } from 'src/directives/dropdown.directive';
@@ -16,13 +14,12 @@ import { AuthInterceptorService } from 'src/services/auth-interceptor.service';
 
 import { AppRoutesModule } from "./app-routing.module";
 import { RecipesModule } from "./recipes/recipes.module";
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     DropdownDirective,
@@ -34,7 +31,8 @@ import { RecipesModule } from "./recipes/recipes.module";
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutesModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
